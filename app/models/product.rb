f9840b288a19824be9a4ccc1712a58d0de1d027f
Zepa Dorji <zepadorji222@gmail.com>
2021-06-23 23:05:58 +0600

    image add to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :category
+  has_many_attached :images
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+
 end
