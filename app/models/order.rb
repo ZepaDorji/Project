e1bdeac3e04559a11133bd5890c5b7a873be42ae
Zepa Dorji <zepadorji222@gmail.com>
2021-06-25 09:37:52 +0600

    validatation add on order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :product
+  validates :name, presence: true
+  validates :cid, length: { is: 11 }
+  validates :contact_number, presence: true
+  validates :quantity, presence: true
+  validates :email, presence: true
+  validates :address, length: {minimum: 10, maximum:50}
+
 end
